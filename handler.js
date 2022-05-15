ae5e0375a886edba74e358983364200bc291a298
Leonardo Carrasco <lcarrasco@workstride.com>
2022-05-15 16:49:04 -0300

    Basic but fully functional lambda with integration to a SQS queue
    Added handler for hacerPedido (sender)

---
+++ handler.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const crypto = require('crypto');
+
+const AWS = require('aws-sdk')
+
+const QUEUE_URL = process.env.PENDING_ORDER_QUEUE
+const REGION = process.env.REGION
+
+AWS.config.update({region:REGION});
+
+var sqs = new AWS.SQS();
+
+module.exports.hacerPedido = (event, context, callback) => {
+  console.log('HacerPedido fue llamada');
+  const orderId = crypto.randomUUID();
+
+  const params = {
+    MessageBody: JSON.stringify({ orderId: orderId }),
+    QueueUrl: QUEUE_URL
+  };
+
+  sqs.sendMessage(params, function(err, data) {
+    if(err) {
+      console.err("Ocurrio un error. " + err);
+      sendResponse(500, err, callback);
+    } else {
+      console.log("Mensaje enviado, (" + data.MessageId + "), Orden " + orderId);
+      const message = {
+        orderId: `${orderId}`
+      };
+      sendResponse(200, message, callback);
+    }
+  });
+};
+
+function sendResponse(statusCode, message, callback) {
+  
+  const response = {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+
+  callback(null, response);
+
+}
